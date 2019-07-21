16435c7a604d22c784b5f27f34335815f5f6cb90
Regis <sidger@zoho.com>
2019-07-21 17:48:32 +0200

    rename properly Pair struct

--- src/data_struct.rs
+++ src/data_struct.rs
@@ -3,11 +3,11 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Clone)]
-pub struct Node {
+struct Node {
     node_id: u32,
     timestamp: u64,
-    // The edges of the graph are stored directly in the node in the form fof vecotr of node_id
-    // Since the graph is fairly simple and restricted in size it simplify its construction and exploration (at the cost of performance)
+
+    // These 2 vectors represent the edges of the node in both direction
     parents: Vec<u32>,
     children: Vec<u32>,
 }
@@ -35,15 +35,14 @@ impl Node {
     }
 }
 
-struct Pair {
-    p0: u32,
+struct Edge {
     p1: u32,
     p2: u32,
 }
 
 #[derive(Clone)]
 pub struct Graph {
-    pub nodes: Vec<Node>,
+    nodes: Vec<Node>,
 }
 
 impl Graph {
@@ -65,19 +64,18 @@ impl Graph {
         // This gives the number of noce which is not necessary in Rust to parse a file
         let _res = reader.read_line(&mut first_line);
         let _res = match _res {
-            Ok(usize) => usize, 
-            Err(error) => {
-                panic!("Error reading first line: {:?}", error)
-            },
+            Ok(usize) => usize,
+            Err(error) => panic!("Error reading first line: {:?}", error),
         };
         // Remove trailing new line before parsing
         let first_line = first_line.trim_end();
-        let num_nodes : usize = first_line.parse().unwrap();
+        let num_nodes: usize = first_line.parse().unwrap();
 
         // First node is a special case
         self.add_node(0, 0);
 
-        let mut tmp_vec: Vec<Pair> = Vec::new();
+        // Store the vertices set in a temporary Vec
+        let mut tmp_vec: Vec<Edge> = Vec::new();
         let mut index = 1;
         for l in reader.lines() {
             let line: String = l.unwrap();
@@ -90,8 +88,7 @@ impl Graph {
             tmp_node_id_p1 = tmp_node_id_p1 - 1;
             tmp_node_id_p2 = tmp_node_id_p2 - 1;
 
-            tmp_vec.push(Pair {
-                p0: index,
+            tmp_vec.push(Edge {
                 p1: tmp_node_id_p1,
                 p2: tmp_node_id_p2,
             });
@@ -99,40 +96,50 @@ impl Graph {
             index = index + 1;
         }
 
-        // Construct the graph
-        for p in tmp_vec {
+        // Construct the graph from the vertices set
+        for (pos, p) in tmp_vec.iter().enumerate() {
             // -1 offset because nodes_id starts at 1 and vector index starts at 0
-            self.nodes[(p.p0) as usize].add_parent(p.p1);
-            self.nodes[(p.p0) as usize].add_parent(p.p2);
-            self.nodes[(p.p1) as usize].add_child(p.p0);
-            self.nodes[(p.p2) as usize].add_child(p.p0);
+            self.nodes[(pos + 1) as usize].add_parent(p.p1);
+            self.nodes[(pos + 1) as usize].add_parent(p.p2);
+            self.nodes[(p.p1) as usize].add_child((pos as u32) + 1);
+            self.nodes[(p.p2) as usize].add_child((pos as u32) + 1);
         }
 
         // Do not forget node 0
         // Sanity check
-        assert_eq!(num_nodes+1, self.nodes.len());
+        assert_eq!(num_nodes + 1, self.nodes.len());
     }
 
-    // Unless we don't identical references on a given nodes there are always 2 references per node 
+    // Get number of leaf in the graph
+    pub fn get_num_of_leaf(&self) -> u32 {
+        let mut num_leaf = 0;
+        for n in 1..self.nodes.len() {
+            if self.nodes[n].children.len() == 0 {
+                num_leaf = num_leaf + 1;
+            }
+        }
+        num_leaf
+    }
+
+    // Unless we don't identical references on a given nodes there are always 2 references per node
     // This formulas only depends on the number of node
-    pub fn get_avg_reference_per_node(&self) -> f32{
-        let num_ref : f32 = 2.0*(self.nodes.len() as f32 - 1.0 ) / (self.nodes.len() as f32) ;
+    pub fn get_avg_reference_per_node(&self) -> f32 {
+        let num_ref: f32 = 2.0 * (self.nodes.len() as f32 - 1.0) / (self.nodes.len() as f32);
         num_ref
     }
 
-    pub fn get_avg_depth(&mut self) -> f32{
-        let mut sum_depth : f32 = 0.0;
+    pub fn get_avg_depth(&mut self) -> f32 {
+        let mut sum_depth: f32 = 0.0;
         // Use index as node_id to avoid borrowing self
         for n in 0..self.nodes.len() {
-           sum_depth = sum_depth + (Graph::get_depth(&mut self.nodes, n as u32)) as f32; 
+            sum_depth = sum_depth + (Graph::get_depth(&mut self.nodes, n as u32)) as f32;
         }
         sum_depth = sum_depth / (self.nodes.len() as f32);
         sum_depth
     }
 
-
-    pub fn get_avg_tx_per_depth(&mut self) -> f32{
-        let mut avg_tx : Vec<f32> = Vec::new();
+    pub fn get_avg_tx_per_depth(&mut self) -> f32 {
+        let mut avg_tx: Vec<f32> = Vec::new();
         let mut curr_depth = 0;
 
         for n in 1..self.nodes.len() {
@@ -142,7 +149,7 @@ impl Graph {
                 curr_depth = d;
                 avg_tx.push(n_tx);
             } else {
-                avg_tx[d-1] = avg_tx[d-1]+n_tx;
+                avg_tx[d - 1] = avg_tx[d - 1] + n_tx;
             }
         }
         let mut sum: f32 = avg_tx.iter().sum();
@@ -154,7 +161,7 @@ impl Graph {
         println!("parents ->nodeid ->children");
         for node in &self.nodes {
             let nspace = min(8 - (node.parents.len()) * 3, 6);
-            let mut depth = Graph::get_depth(&mut self.nodes.clone(), node.node_id);
+            let depth = Graph::get_depth(&mut self.nodes.clone(), node.node_id);
             println!(
                 "{parents:?}{a:>w$}->{nodeid}({depth}){b:filler$}->{children:?}",
                 parents = node.parents,
@@ -169,6 +176,7 @@ impl Graph {
         }
     }
 
+    // This function is not efficient as it has to check all the way to the root node before being able to determine its depth
     fn get_depth(nodes: &mut [Node], node_id: u32) -> u32 {
         let parents = &mut nodes[node_id as usize].parents;
         let mut d_vec: Vec<u32> = Vec::new();
@@ -176,17 +184,16 @@ impl Graph {
             let par = parents.clone();
             // println!("{:?} : {}", parents, node_id);
             for p in par {
-                let d = Graph::get_depth(nodes, p)+1;
-                d_vec.push(d); 
+                let d = Graph::get_depth(nodes, p) + 1;
+                d_vec.push(d);
             }
         }
         // println!("d_vec={:?}", d_vec);
         let mut d = 0;
         for d_val in d_vec {
-            if d == 0  {
+            if d == 0 {
                 d = d_val;
-            }
-            else if d_val < d {
+            } else if d_val < d {
                 d = d_val;
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use data_struct::Graph;
 fn main() {
     println!("Hello, world!");
     let mut graph: Graph = Graph::new();
-    let filename = "database.txt".to_string();
+    let filename = "database1.txt".to_string();
     graph.parse(filename);
     graph.show();
-
 
     let avg_depth = graph.get_avg_depth();
     println!("avg_depth={}", avg_depth);
 
-    let avg_tx= graph.get_avg_tx_per_depth();
+    let avg_tx = graph.get_avg_tx_per_depth();
     println!("avg_tx={}", avg_tx);
 
-    let avg_ref= graph.get_avg_reference_per_node();
+    let avg_ref = graph.get_avg_reference_per_node();
     println!("avg_ref={}", avg_ref);
+
+    let num_leaf = graph.get_num_of_leaf();
+    println!("num_leaf={}", num_leaf);
 }
